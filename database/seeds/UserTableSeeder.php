ea4b9acf548b29b42fed677db2a12400f2c7ad42
Dragon DeveloperTra <you@example.com>
2019-05-02 18:16:06 +0200

    first commit:user login

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'username' => 'tamara',
+            'email' => 'tamara@test.com',
+            'password' => bcrypt('123456')
+        ]);
+    }
+}
