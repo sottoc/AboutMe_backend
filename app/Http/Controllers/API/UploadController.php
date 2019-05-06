16dde281dc029ac7455d2c85c95f5d1ad3be415c
Dragon DeveloperTra <you@example.com>
2019-05-06 19:16:37 +0200

    update /api/upload/

--- app/Http/Controllers/API/UploadController.php
+++ app/Http/Controllers/API/UploadController.php
@@ -16,16 +16,30 @@ class UploadController extends Controller
     {
         $image_url = null;
         if($request->has('file')) {
-            // Get file name with extensions
             $file = $request->file;
-            $fileName = $file->getClientOriginalName();
-            $extension = $file->getClientOriginalExtension();
-            $fileNameToStore = $fileName.'_'.time().'.'.$extension;
+            if (preg_match('/^data:image\/(\w+);base64,/', $file)) {
+                $data = substr($file, strpos($file, ',') + 1);
+                $data = base64_decode($data);
+                $file_type = $request->file_type;
+                $extension = explode("/", $file_type)[1];
+                $filename = $request->file_name;
+                // Filename to store
+                $fileNameToStore = $filename.'_'.time().'.'.$extension;
+                // Upload Image
+                Storage::disk('local')->put('public/uploads/'.$fileNameToStore, $data);
+                $image_url = Storage::url('public/uploads/'.$fileNameToStore);
+            }
 
-            //Move Uploaded File
-            $destinationPath = 'uploads';
-            $file->move($destinationPath, $fileNameToStore);
-            $image_url = $destinationPath.'/'.$fileNameToStore;
+            // // Get file name with extensions
+            // $file = $request->file;
+            // $fileName = $file->getClientOriginalName();
+            // $extension = $file->getClientOriginalExtension();
+            // $fileNameToStore = $fileName.'_'.time().'.'.$extension;
+
+            // //Move Uploaded File
+            // $destinationPath = 'uploads';
+            // $file->move($destinationPath, $fileNameToStore);
+            // $image_url = $destinationPath.'/'.$fileNameToStore;
         }
 
         return $image_url;
